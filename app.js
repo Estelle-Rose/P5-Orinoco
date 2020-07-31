e3a0f58477dd74b4b290e1436320b795ec650b85
Estelle-Rose <serenetzen@gmail.com>
2020-07-31 10:36:02 +0200

    gestion node server paramètres

--- app.js
+++ app.js
@@ -34,5 +34,8 @@ app.use(bodyParser.json());
 app.use('/api/cameras', cameraRoutes);
 app.use('/api/teddies', teddyRoutes);
 app.use('/api/furniture', furnitureRoutes);
-
+app.use(express.static("public"));
+app.get('/', function(req,res) {
+  res.sendFile( __dirname + "/" + "index.html" )
+});
 module.exports = app;
